f6e6adcee70458545c6f63f0b88e3d9a3cabf82a
EleganTT <dmitriy.lee.developer@gmail.com>
2021-12-10 14:10:18 +0600

    NEW

--- homeWork10/YandexViewController.swift
+++ homeWork10/YandexViewController.swift
@@ -14,13 +14,21 @@ class YandexViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let mapKit = YMKMapKit.sharedInstance()
+        let scale = UIScreen.main.scale
+        
         mapView = YMKMapView(frame: CGRect(x: 0, y: 48, width: view.frame.width, height: view.frame.height - 130))
         
         
         mapView?.mapWindow.map.move(with:
-                                        YMKCameraPosition(target: YMKPoint(latitude: 43.198672, longitude: 76.869073), zoom: 14, azimuth: 0, tilt: 0))
+                                        YMKCameraPosition(target: YMKPoint(latitude: 0, longitude: 0), zoom: 14, azimuth: 0, tilt: 0))
         
+
+        let userLocationLayer = mapKit.createUserLocationLayer(with: (mapView?.mapWindow)!)
+        userLocationLayer.setVisibleWithOn(true)
+        userLocationLayer.isHeadingEnabled = true
+        userLocationLayer.setAnchorWithAnchorNormal(CGPoint(x: 0.5 * mapView!.frame.size.width * scale, y: 0.5 * mapView!.frame.size.height * scale), anchorCourse: CGPoint(x: 0.5 * mapView!.frame.size.width * scale, y: 0.83 * mapView!.frame.size.height * scale))
 
         
         view.addSubview(mapView!)
